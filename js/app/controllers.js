428ca6cacd6c7cfc52fefc4dd78142d3d032b3c9
Igor Kotenko <ink_mail@yahoo.com>
2013-06-04 17:03:36 +0400

    models update

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -2,15 +2,42 @@
 
 /* Controllers */
 
-function CalculatorCtrl($scope) {
+function CalculatorCtrl($scope, CarCompanys, CarModels, CarModifs, CarYears) {
     $scope.car = {
         price: 813670,
         casco: 0,
-        osago: 0
+        osago: 0,
+        companyId: undefined,
+        modelId: undefined,
+        modifId: undefined,
+        yearId: undefined
     }
 
-    $scope.summ = function() {
+    $scope.carCompanyList = CarCompanys;
+    $scope.carModelList = CarModels;
+    $scope.carModifList = CarModifs;
+    $scope.carYearList = CarYears;
+
+    $scope.summ = function () {
         return parseFloat($scope.car.price) + parseFloat($scope.car.osago) + parseFloat($scope.car.casco);
+    }
+
+    $scope.updateCompany = function () {
+        $scope.car.modelId = undefined;
+        $scope.updateModel();
+    }
+
+    $scope.updateModel = function () {
+        $scope.car.modifId = undefined;
+        $scope.updateModif();
+    }
+
+    $scope.updateModif = function () {
+        $scope.car.yearId = undefined;
+        $scope.updateYear();
+    }
+
+    $scope.updateYear = function () {
     }
 
 }
--- js/app/services.js
+++ js/app/services.js
@@ -7,7 +7,47 @@ var seedAppServiceModule = angular.module('seedApp.services', []);
 seedAppServiceModule.value('version', '0.1');
 
 seedAppServiceModule.
-    factory('Task', function($resource){
+    factory('Task', function ($resource) {
         var taskData = $resource('http://rciserv01/rest/bpm/htm/v1/tasks/filter', {}, {});
         return taskData;
+    });
+
+seedAppServiceModule.
+    factory('CarCompanys', function ($resource) {
+        return [
+            {id: "c1", name: "Марка"},
+            {id: "c2", name: "DMC"},
+            {id: "c3", name: "Ваз"},
+            {id: "c4", name: "Nissan"}
+        ];
+    });
+
+seedAppServiceModule.
+    factory('CarModels', function ($resource) {
+        return [
+            {id: "cm1", name: "Модель1"},
+            {id: "cm2", name: "Модель2"},
+            {id: "cm3", name: "Модель3"},
+            {id: "cm4", name: "Модель4"}
+        ]
+    });
+
+seedAppServiceModule.
+    factory('CarModifs', function ($resource) {
+        return [
+            {id: "m1", name: "Модификация1"},
+            {id: "m2", name: "Модификация2"},
+            {id: "m3", name: "Модификация3"},
+            {id: "m4", name: "Модификация4"}
+        ]
+    });
+
+seedAppServiceModule.
+    factory('CarYears', function ($resource) {
+        return [
+            {id: "y2000", name: "2000"},
+            {id: "y2001", name: "2001"},
+            {id: "y2011", name: "2011"},
+            {id: "y2013", name: "2013"}
+        ]
     });
