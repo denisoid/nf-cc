ba157a206e5ba1dc16b2ebfc9c03db238db302fc
Igor Kotenko <ink_mail@yahoo.com>
2013-06-05 15:58:56 +0400

    new directive - slider

--- js/app/directives.js
+++ js/app/directives.js
@@ -4,15 +4,24 @@
 
 var seedAppDirectives = angular.module('seedApp.directives', []);
 
-seedAppDirectives.directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+seedAppDirectives.directive('appVersion', ['version', function (version) {
+    return function (scope, elm, attrs) {
+        elm.text(version);
     };
-  }]);
+}]);
 
-seedAppDirectives.directive('slider', function() {
+seedAppDirectives.directive('slider', function () {
     return {
-        restrict: "A",
-        template: ""
+        link: function (scope, elem, attrs) {
+            $(elem).slider({
+                range: true,
+                min: scope.days[1],
+                max: scope.days[scope.days.length - 1],
+                values: [scope.days[0], scope.days[scope.days.length - 1]],
+                slide: function (event, ui) {
+                    console.log(ui.values[0], ui.values[1]);
+                }
+            });
+        }
     }
 });
