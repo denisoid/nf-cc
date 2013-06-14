3460fa2dafc1fa422906076e083d7efaa726cfae
Igor Kotenko <ink_mail@yahoo.com>
2013-06-14 18:29:44 +0400

    additional services implemented

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -19,9 +19,9 @@ function CarConfigurationCtrl($scope, $filter, LoanProducts, Packaging_LoanProdu
 
     //init
 
-    $scope.markList = Marks.query({}, function() {
+    $scope.markList = Marks.query({}, function () {
         $scope.updateMark();
-    } );
+    });
     $scope.selMarkList = [];
     $scope.modelList = Models.query();
     $scope.selModelList = [];
@@ -44,7 +44,7 @@ function CarConfigurationCtrl($scope, $filter, LoanProducts, Packaging_LoanProdu
     $scope.car = CarConfiguration;
 
     $scope.updateMark = function () {
-        $scope.selMarkList = $filter('filter')($scope.markList, function(element) {
+        $scope.selMarkList = $filter('filter')($scope.markList, function (element) {
             return element.used == $scope.car.used;
         });
         $scope.car.markId = undefined;
@@ -52,7 +52,7 @@ function CarConfigurationCtrl($scope, $filter, LoanProducts, Packaging_LoanProdu
     }
 
     $scope.updateModel = function () {
-        $scope.selModelList = $filter('filter')($scope.modelList, function(element) {
+        $scope.selModelList = $filter('filter')($scope.modelList, function (element) {
             return element.markId == $scope.car.markId;
         });
         $scope.car.modelId = undefined;
@@ -60,7 +60,7 @@ function CarConfigurationCtrl($scope, $filter, LoanProducts, Packaging_LoanProdu
     }
 
     $scope.updatePackaging = function () {
-        $scope.selPackagingList = $filter('filter')($scope.packagingList, function(element) {
+        $scope.selPackagingList = $filter('filter')($scope.packagingList, function (element) {
             return element.modelId == $scope.car.modelId;
         });
         $scope.car.packagingId = undefined;
@@ -72,18 +72,18 @@ function CarConfigurationCtrl($scope, $filter, LoanProducts, Packaging_LoanProdu
 
     $scope.updateCarForPackaging = function () {
         var searchedId = $scope.car.packagingId;
-        if(searchedId == undefined) {
+        if (searchedId == undefined) {
             return;
         }
         var packaging = undefined;
-        for(var ti = 0, len = $scope.selPackagingList.length; ti < len; ti++) {
+        for (var ti = 0, len = $scope.selPackagingList.length; ti < len; ti++) {
             var pack = $scope.selPackagingList[ti];
-            if(pack.id == searchedId) {
+            if (pack.id == searchedId) {
                 packaging = pack;
                 break;
             }
         }
-        if(packaging == undefined) {
+        if (packaging == undefined) {
             $scope.car.price = undefined;
             $scope.car.discount = undefined;
         } else {
@@ -92,8 +92,8 @@ function CarConfigurationCtrl($scope, $filter, LoanProducts, Packaging_LoanProdu
         }
     }
 
-    $scope.$watch('car.packagingId', function(newVal, oldVal) {
-        if(newVal === oldVal) return;
+    $scope.$watch('car.packagingId', function (newVal, oldVal) {
+        if (newVal === oldVal) return;
         $scope.updateCarForPackaging();
     });
 }
@@ -118,8 +118,8 @@ function LoanProgramSelectionCtrl($scope, LoanProducts, Packaging_LoanProduct, C
 
 
     $scope.addToSelectedOfferList = function (idx) {
-        var foundIdx = $scope.selectedOfferList.indexOf($scope.currentOfferList[idx]);
-        if(foundIdx == -1) {
+        var foundIdx = $.inArray($scope.currentOfferList[idx], $scope.selectedOfferList);
+        if (foundIdx == -1) {
             $scope.selectedOfferList.push($scope.currentOfferList[idx]);
         }
     }
@@ -128,43 +128,43 @@ function LoanProgramSelectionCtrl($scope, LoanProducts, Packaging_LoanProduct, C
         $scope.selectedOfferList.splice(idx, 1);
     }
 
-    $scope.$watch('car.packagingId', function(newVal, oldVal) {
-        if(newVal === oldVal) return;
+    $scope.$watch('car.packagingId', function (newVal, oldVal) {
+        if (newVal === oldVal) return;
         $scope.filterLoanProductListForPack();
         $scope.filterLoanProductListForCriteria();
         $scope.updateCurrentOfferList();
     });
 
     $scope.$watch('car.price', function (newVal, oldVal) {
-        if(newVal === oldVal) return;
+        if (newVal === oldVal) return;
         $scope.initialPaymentPercent = Math.round($scope.initialPayment * 100 / $scope.car.price);
         $scope.filterLoanProductListForCriteria();
         $scope.updateCurrentOfferList();
     });
 
     $scope.$watch('initialPayment', function (newVal, oldVal) {
-        if(newVal === oldVal) return;
+        if (newVal === oldVal) return;
         $scope.initialPaymentPercent = Math.round($scope.initialPayment * 100 / $scope.car.price);
         $scope.filterLoanProductListForCriteria();
         $scope.updateCurrentOfferList();
     });
 
     $scope.$watch('months', function (newVal, oldVal) {
-        if(newVal === oldVal) return;
+        if (newVal === oldVal) return;
         $scope.filterLoanProductListForCriteria();
         $scope.updateCurrentOfferList();
     })
 
     $scope.filterLoanProductListForPack = function () {
 
-        if($scope.car.packagingId == undefined) {
+        if ($scope.car.packagingId == undefined) {
             $scope.loanProductForPackList = $scope.loanProductList;
             return;
         }
 
         var filteredProductList = [];
 
-        for(var ti = 0, len = $scope.packaging_loanProductList.length; ti < len; ti++) {
+        for (var ti = 0, len = $scope.packaging_loanProductList.length; ti < len; ti++) {
             var packId = $scope.packaging_loanProductList[ti].packId;
             if ($scope.car.packagingId == packId) {
                 filteredProductList.push($scope.packaging_loanProductList[ti].productId);
@@ -172,7 +172,7 @@ function LoanProgramSelectionCtrl($scope, LoanProducts, Packaging_LoanProduct, C
         }
 
         $scope.loanProductForPackList = $filter('filter')($scope.loanProductList, function (element) {
-                for(var tj = 0, len = filteredProductList.length; tj < len; tj++) {
+                for (var tj = 0, len = filteredProductList.length; tj < len; tj++) {
                     var productId = filteredProductList[tj];
                     if (element.id == productId) {
                         return true;
@@ -200,11 +200,11 @@ function LoanProgramSelectionCtrl($scope, LoanProducts, Packaging_LoanProduct, C
 
     $scope.updateCurrentOfferList = function () {
         $scope.currentOfferList = [];
-        for( var i = 0, len = $scope.loanProductForCriteriaList.length; i < len; i++) {
+        for (var i = 0, len = $scope.loanProductForCriteriaList.length; i < len; i++) {
             var product = $scope.loanProductForCriteriaList[i];
             var creditValue = $scope.car.price - $scope.initialPayment;
-            var overPayment = creditValue * product.rate * $scope.months/1200;
-            var monthPayment = (creditValue + overPayment)/$scope.months;
+            var overPayment = Math.round(creditValue * product.rate * $scope.months / 1200);
+            var monthPayment = Math.round((creditValue + overPayment) / $scope.months);
             var offer = {
                 id: product.id,
                 name: product.name,
@@ -214,9 +214,44 @@ function LoanProgramSelectionCtrl($scope, LoanProducts, Packaging_LoanProduct, C
                 months: $scope.months,
                 monthPayment: monthPayment,
                 overPayment: overPayment,
-                rate: product.rate
+                rate: product.rate,
+                servicePrice:125000
             };
             $scope.currentOfferList.push(offer);
         }
     }
+}
+
+function OfferCtrl($scope, CarConfiguration, $filter) {
+    $scope.serviceList = [
+        {"id": "1", "selected": false, mandatory: false, "name": "Расш. гарантия", "ctype": "fixed", "cost": "3000"},
+        {"id": "2", "selected": false, mandatory: false, "name": "Помощь на дорогах", "ctype": "fixed", "cost": "3000"},
+        {"id": "3", "selected": false, mandatory: false, "name": "Защита кредита", "ctype": "fixed", "cost": "125000"},
+        {"id": "4", "selected": false, mandatory: false, "name": "ДСАГО", "ctype": "input", "cost": "125000"},
+        {"id": "5", "selected": true, mandatory: true, "name": "КАСКО", "ctype": "input", "cost": "125000"}
+    ];
+
+    $scope.updateOffer = function () {
+        var offer = $scope.offer;
+        var price = 0;
+        for (var ti = 0, len = $scope.serviceList.length; ti < len; ti++) {
+            var service = $scope.serviceList[ti];
+            if (service.selected == true) {
+                var cost = parseFloat(service.cost)
+                if(!isNaN(cost)) {
+                    price = price + cost;
+                }
+            }
+        }
+        offer.servicePrice = price;
+
+        offer.creditValue = offer.price - offer.initialPayment + offer.servicePrice;
+        offer.overPayment = Math.round(offer.creditValue * offer.rate * offer.months / 1200);
+        offer.monthPayment = Math.round((offer.creditValue + offer.overPayment) / offer.months);
+    };
+
+    $scope.$watch('serviceList', function (newVal, oldVal) {
+        if (newVal === oldVal) return;
+        $scope.updateOffer();
+    });
 }
