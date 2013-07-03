64e2bf2d2a94016a3664084e0996f3f56ff862e9
Igor Kotenko <ink_mail@yahoo.com>
2013-07-03 18:39:39 +0400

    paging for upgrades

--- js/app/controllers/UpgradeCtrl.js
+++ js/app/controllers/UpgradeCtrl.js
@@ -6,10 +6,15 @@
  * Time: 14:19
  */
 function UpgradeCtrl($scope) {
-    $scope.carUpgradeList = [{id:1},{id:2},{id:3}];
-    $scope.serviceUpgradeList = [{id:1},{id:2},{id:3}];
+
+    $scope.carUpgrade = new ListWithPaging([{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7}], 3);
+    $scope.serviceUpgrade = new ListWithPaging([{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7}], 3);
 
     $scope.updateUpgrades = function () {
-
+        $scope.carUpgrade.setup([{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7}]);
+        $scope.serviceUpgrade.setup([{id:1},{id:2},{id:3},{id:4},{id:5},{id:6},{id:7}]);
     }
+
+
+
 }
---
+++ js/app/utility.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * User: ikotenko
+ * Date: 03.07.13
+ * Time: 17:53
+ */
+var ListWithPaging = function (list, pageSize) {
+    this.list = list;
+    this.length = list.length;
+    this.start = 0;
+    this.pageSize = pageSize;
+    this.end = this.start + this.pageSize;
+    if(this.end > this.length) {
+        this.end = this.length;
+    }
+    this.currentPage = this.list.slice(this.start, this.end);
+}
+
+ListWithPaging.prototype.isLeft = function () {
+    return this.start > 0;
+}
+
+ListWithPaging.prototype.isRight = function () {
+    return (this.start + this.pageSize) < this.length
+}
+
+ListWithPaging.prototype.left = function () {
+    if(this.start >= this.pageSize) {
+        this.start -= this.pageSize;
+        this.end = this.start + this.pageSize;
+        if(this.end > this.length) {
+            this.end = this.length;
+        }
+        this.currentPage = this.list.slice(this.start, this.end);
+    }
+}
+
+ListWithPaging.prototype.right = function () {
+    if(this.start + this.pageSize < this.length) {
+        this.start += this.pageSize;
+        this.end = this.start + this.pageSize;
+        if(this.end > this.length) {
+            this.end = this.length;
+        }
+        this.currentPage = this.list.slice(this.start, this.end);
+    }
+}
+
+ListWithPaging.prototype.setup = function(list) {
+    this.list = list;
+    this.init();
+}
+
+ListWithPaging.prototype.init = function () {
+    this.length = list.length;
+    this.start = 0;
+    this.pageSize = pageSize;
+    this.end = this.index + this.pageSize;
+    if(this.end > this.length) {
+        this.end = this.length;
+    }
+    this.currentPage = this.list.slice(this.start, this.end);
+}
