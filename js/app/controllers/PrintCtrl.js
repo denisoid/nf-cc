1fc1cea5ad7b79a0b37398fc58b7eef4c97f05cc
Denis Oleshkevich <doleshkevich@neoflex.ru>
2013-07-09 16:14:55 +0400

    print grid

--- js/app/controllers/PrintCtrl.js
+++ js/app/controllers/PrintCtrl.js
@@ -9,4 +9,10 @@ function PrintCtrl($scope, $window) {
     $scope.printOffer = function() {
         $window.print();
     }
+
+
+    $scope.printGrid = function() {
+        var win = window.open("grid.html");
+    }
+
 }
