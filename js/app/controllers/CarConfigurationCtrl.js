fb8c198ffff1e9e9caddba8545b05d939624d591
Igor Kotenko <ink_mail@yahoo.com>
2013-07-09 09:38:35 +0400

    refactoring for car details

--- js/app/controllers/CarConfigurationCtrl.js
+++ js/app/controllers/CarConfigurationCtrl.js
@@ -9,7 +9,6 @@ function CarConfigurationCtrl($scope, $filter, LoanProducts, Packaging_LoanProdu
 
     //init
     $scope.car = CalculatorData.calculation.car;
-    $scope.current = {mark: null, model: null, pack: null};
 
     $scope.selMarkList = [];
     $scope.selModelList = [];
@@ -40,77 +39,43 @@ function CarConfigurationCtrl($scope, $filter, LoanProducts, Packaging_LoanProdu
             return element.used == $scope.car.used;
         });
         if ($scope.selMarkList.length > 0) {
-            $scope.current.mark = $scope.selMarkList[0];
+            $scope.car.mark = $scope.selMarkList[0];
         } else {
-            $scope.current.mark = null;
+            $scope.car.mark = null;
         }
         $scope.updateModel();
     }
 
     $scope.updateModel = function () {
-
+        if($scope.car.mark == null) {
+            $scope.car.model = null;
+            return;
+        }
         if ($scope.modelList.length == 0) return;
         $scope.selModelList = $filter('filter')($scope.modelList, function (element) {
-            return element.markId == $scope.current.mark.id;
+            return element.markId == $scope.car.mark.id;
         });
         if ($scope.selModelList.length > 0) {
-            $scope.current.model = $scope.selModelList[0];
+            $scope.car.model = $scope.selModelList[0];
         } else {
-            $scope.current.model = null;
+            $scope.car.model = null;
         }
         $scope.updatePackaging();
     }
 
     $scope.updatePackaging = function () {
-        if ($scope.packagingList.length == 0) return;
-        $scope.selPackagingList = $filter('filter')($scope.packagingList, function (element) {
-            return element.modelId == $scope.current.model.id;
-        });
-        if ($scope.selPackagingList.length > 0) {
-            $scope.current.pack = $scope.selPackagingList[0];
-        } else {
-            $scope.current.pack = null;
-        }
-    }
-
-    $scope.updateCarForPackaging = function () {
-        var searchedId = $scope.current.pack.id;
-        if (searchedId == null) {
+        if($scope.car.model == null) {
+            $scope.car.pack = null;
             return;
         }
-        var packaging = null;
-        for (var ti = 0, len = $scope.selPackagingList.length; ti < len; ti++) {
-            var pack = $scope.selPackagingList[ti];
-            if (pack.id == searchedId) {
-                packaging = pack;
-                break;
-            }
-        }
-        if (packaging == null) {
-            $scope.car.price = null;
-            $scope.car.discount = null;
+        if ($scope.packagingList.length == 0) return;
+        $scope.selPackagingList = $filter('filter')($scope.packagingList, function (element) {
+            return element.modelId == $scope.car.model.id;
+        });
+        if ($scope.selPackagingList.length > 0) {
+            $scope.car.pack = $scope.selPackagingList[0];
         } else {
-            $scope.car.price = packaging.cost;
-            $scope.car.discount = packaging.discount;
+            $scope.car.pack = null;
         }
     }
-
-    $scope.$watch('current.mark', function (newVal, oldVal) {
-        if (newVal === oldVal) return;
-        $scope.car.markId = $scope.current.mark.id;
-        $scope.car.markName = $scope.current.mark.name;
-    });
-
-    $scope.$watch('current.model', function (newVal, oldVal) {
-        if (newVal === oldVal) return;
-        $scope.car.modelId = $scope.current.model.id;
-        $scope.car.modelName = $scope.current.model.name;
-    });
-
-    $scope.$watch('current.pack', function (newVal, oldVal) {
-        if (newVal === oldVal) return;
-        $scope.updateCarForPackaging();
-        $scope.car.packagingId = $scope.current.pack.id;
-        $scope.car.packagingName = $scope.current.pack.name;
-    });
 }
--- js/app/services.js
+++ js/app/services.js
@@ -43,33 +43,15 @@ seedAppServiceModule.
     });
 
 seedAppServiceModule.
-    factory('CarConfiguration', function ($resource) {
-        return {
-            used: false,
-            price: undefined,
-            discount: undefined,
-            markId: undefined,
-            modelId: undefined,
-            packagingId: undefined,
-            yearId: undefined
-        }
-
-    });
-
-seedAppServiceModule.
     factory('CalculatorData', function () {
         return {
             calculation: {
                 car: {
                     used: false,
-                    price: 0,
-                    discount: 0,
-                    markId: null,
-                    modelId: null,
-                    packagingId: null,
-                    yearId: null,
-                    markName: null,
-                    modelName: null
+                    mark: null,
+                    model: null,
+                    pack: null,
+                    yearId: null
                 },
                 loanproduct: null,
                 offer: null
@@ -80,14 +62,10 @@ seedAppServiceModule.
                 this.calculationList.push({
                     car: {
                         used: this.calculation.car.used,
-                        price: this.calculation.car.price,
-                        discount: this.calculation.car.discount,
-                        markId: this.calculation.car.markId,
-                        modelId: this.calculation.car.modelId,
-                        packagingId: this.calculation.car.packagingId,
-                        yearId: this.calculation.car.yearId,
-                        markName: this.calculation.car.markName,
-                        modelName: this.calculation.car.modelName
+                        mark: this.calculation.car.mark,
+                        model: this.calculation.car.model,
+                        pack: this.calculation.car.pack,
+                        yearId: this.calculation.car.yearId
                     },
                     loanproduct: null,
                     offer: this.calculation.offer
